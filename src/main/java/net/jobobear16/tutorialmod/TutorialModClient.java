3ec985c0c3dac406e768a7228e3560a1b9bf9729
JoboBear16 <83785308+JoboBear16@users.noreply.github.com>
2026-07-02 17:05:00 -0400

    Initial commit

---
+++ src/main/java/net/jobobear16/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.jobobear16.tutorialmod;
+
+import ca.weblite.objc.Client;
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
